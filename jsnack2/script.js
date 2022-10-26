522d4be1b7b9ec2ed04d84a5c623d9cf5bbe58c9
Seme87 <massimomonteleone@hotmail.com>
2022-10-26 11:04:28 +0200

    first commit

---
+++ jsnack2/script.js
@@ -0,0 +1,11 @@
+const firstWord = prompt("inserisci la prima parola");
+const secondtWord = prompt("inserisci la prima parola");
+
+if(firstWord.length < secondtWord.length ){
+    console.log(firstWord)
+    console.log(secondtWord)
+}else if (firstWord.length > secondtWord.length) {
+    console.log(secondtWord)
+    console.log(firstWord)
+}
+
